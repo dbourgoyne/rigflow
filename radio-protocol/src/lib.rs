f6c1b18ea255f9fd34726bb5afc6beaf5b09d068
David Bourgoyne <dbourgoyne@gmail.com>
2026-03-24 16:53:00 -0500

    fix snake_case

--- radio-protocol/src/lib.rs
+++ radio-protocol/src/lib.rs
@@ -1,9 +1,24 @@
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Serialize, Deserialize, Clone)]
-#[serde(tag = "type")]
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(tag = "type", rename_all = "snake_case")]
+pub enum ClientMessage {
+    SetFrequency { target_freq_hz: f32 },
+    SetCenterFrequency { center_freq_hz: f32 },
+    SetSideband { sideband: String },
+    SetDemodMode { mode: String },
+    Ping,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(tag = "type", rename_all = "snake_case")]
 pub enum ServerMessage {
     Ready,
+    Pong,
+    FrequencyChanged { target_freq_hz: f32 },
+    CenterFrequencyChanged { center_freq_hz: f32 },
+    SidebandChanged { sideband: String },
+    DemodModeChanged { mode: String },
     StreamConfig {
         audio_sample_rate_hz: f32,
         audio_format: String,
@@ -13,36 +28,7 @@ pub enum ServerMessage {
         target_freq_hz: f32,
         input_sample_rate_hz: f32,
     },
-    FrequencyChanged {
-        target_freq_hz: f32,
-    },
-    CenterFrequencyChanged {
-        center_freq_hz: f32,
-    },
-    DemodModeChanged {
-        mode: String,
-    },
-    SidebandChanged {
-        sideband: String,
-    },
-    UdpAudioOffer {
-        server_udp_port: u16,
-    },
-    Error {
-        message: String,
-    },
-    Info {
-        message: String,
-    },
-    Pong,
-}
-
-#[derive(Debug, Serialize, Deserialize, Clone)]
-#[serde(tag = "type")]
-pub enum ClientMessage {
-    SetFrequency { target_freq_hz: f32 },
-    SetCenterFrequency { center_freq_hz: f32 },
-    SetDemodMode { mode: String },
-    SetSideband { sideband: String },
-    Ping,
+    UdpAudioOffer { server_udp_port: u16 },
+    Info { message: String },
+    Error { message: String },
 }
