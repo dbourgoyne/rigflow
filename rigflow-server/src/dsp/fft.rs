15ba0bd53e2111d51889e96b6dccc1f43000bdf8
David Bourgoyne <dbourgoyne@gmail.com>
2026-04-11 14:18:24 -0500

    server: clean and comment - removed dead code dsp/fft.rs dsp/filter.rs dsp/iq_buffer.rs

--- rigflow-server/src/dsp/fft.rs
+++
@@ -1,3 +0,0 @@
-pub fn fake_fft(size: usize) -> Vec<f32> {
-    (0..size).map(|i| (i as f32).sin()).collect()
-}
--- rigflow-server/src/dsp/mod.rs
+++ rigflow-server/src/dsp/mod.rs
@@ -1,8 +1,5 @@
 pub mod audio;
 pub mod decimator;
 pub mod demod;
-pub mod fft;
-pub mod filter;
-pub mod iq_buffer;
 pub mod pipeline;
 pub mod tuner;
