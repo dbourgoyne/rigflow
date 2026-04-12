f02e4cb02bae89960cfe282402db78f3a76daef2
David Bourgoyne <dbourgoyne@gmail.com>
2026-04-11 21:09:12 -0500

    refactor: move radio session into radio module

--- rigflow-server/src/net/websocket.rs
+++ rigflow-server/src/net/websocket.rs
@@ -14,13 +14,11 @@ use rigflow_protocol::{ClientMessage, ServerMessage};
 use crate::{
     app_state::AppState,
     dsp::demod::{demod_mode_to_string, sideband_to_string, DemodMode, Sideband},
-    radio::types::{ClientId, RadioManagerError, StopReason, WorkerCommand, WorkerStatus},
-    server::{
-        radio_api::{
-            manager_error_to_protocol, parse_acquire_request, radio_summary_to_protocol,
-        },
-        session::SessionState,
+    radio::{
+	types::{ClientId, RadioManagerError, StopReason, WorkerCommand, WorkerStatus},
+	session::SessionState,
     },
+    server::radio_api::{manager_error_to_protocol, parse_acquire_request, radio_summary_to_protocol},
 };
 
 /// Messages that may be sent to a connected WebSocket client.
--- rigflow-server/src/server/mod.rs
+++ rigflow-server/src/server/mod.rs
@@ -2,4 +2,3 @@ pub mod discovery;
 pub mod radio_manager;
 pub mod radio_api;
 pub mod radio_worker;
-pub mod session;
